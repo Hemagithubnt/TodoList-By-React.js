c81814d99a39f4ec175a0daed981e214c33dfb02
Hemant kumar <hmt364134@gmail.com>
2025-12-14 03:34:28 -0800

    Rename Todolist to AddEmployee and update logic

--- src/Component/Todolist/todo.jsx
+++ src/Component/Todolist/todo.jsx
@@ -2,22 +2,24 @@ import { useState } from "react";
 import { v4 as uuidv4 } from 'uuid';
 import Button from '@mui/material/Button';
 
-export default function Todolist() {
+export default function AddEmployee() {
   const [todos, setTodos] = useState([{ task: "sample Task", id: uuidv4() }]);
-  const [newTodo, setNewTodo] = useState("");
+  const [Name, setName] = useState("");
 
-  let AddTask = (e) => {
+
+  let AddName = (e) => {
     e.preventDefault();
-    if (newTodo.trim() !== "") {     // Prevent empty task
-      setTodos([...todos, { task: newTodo, id: uuidv4() }]);
-      setNewTodo("");
+    if (Name.trim() !== "") {     // Prevent empty task
+      setTodos([...todos, { task: Name, id: uuidv4() }]);
+      setName("");
     }
   };
 
-  let handleChange = (event) => {
-    setNewTodo(event.target.value);
+  let handleChange1 = (event) => {
+    setName(event.target.value);
   };
 
+ 
   let handlerDelete = (id) => {
     setTodos((todos) =>
       todos.filter((current) => current.id !== id)
@@ -27,34 +29,33 @@ export default function Todolist() {
     <div className="relative left-130 top-40 w-[20%] border rounded-xl bg-green-300 p-4">
       <form>
         <div className="flex flex-col items-center justify-right gap-5">
-          <h1 className="text-[20px] font-bold">Todo List</h1>
+          <h1 className="text-[20px] font-bold">Add Employee</h1>
+          
           <input
             type="text"
-            placeholder="add a task"
-            value={newTodo}
-            onChange={handleChange}
+            placeholder="Name"
+            value={Name}
+            onChange={handleChange1}
             className="border rounded-2xl p-2 outline-black focus:outline-[.1px] bg-white"
           />
-
           <Button
             className=" !border !rounded-2xl !p-2 !bg-red-500 !text-white"
-            onClick={AddTask}>
-            Add Task
+            onClick={AddName}>
+            Add Employee
           </Button>
 
-          <h4 className="border-b-2 border-t-2 w-full text-center font-[600] text-[20px] ">Todo</h4>
+          <h4 className="border-b-2 border-t-2 w-full text-center font-[600] text-[20px] ">Employee Table</h4>
           <ol type="1">
             {todos.map((todo) => (
               <li key={todo.id} className="flex items-center justify-center gap-3 mb-4 ">
                 {todo.task}
                 {todo.task.trim() !== "" && ( // Show delete only for valid tasks
                   <Button
-                    className=" !border !rounded-2xl !p-2 !bg-amber-300  "
+                    className=" !border !rounded-xl !p-2 !bg-amber-300  "
                     onClick={() => handlerDelete(todo.id)}
-                  > Delete Button</Button>
+                  > Delete</Button>
                  )}
-              </li>
-                
+              </li> 
             ))}
           </ol>
         </div>
